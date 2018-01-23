a2c5b6be38a719cfa4919ea9eb6df4cc409d8aba
ttionn <matt.tian@icloud.com>
2018-01-23 22:23:07 +0800

    Add Chartor

---
+++ Chartor/Source/Chartor.swift
@@ -0,0 +1,13 @@
+//
+//  Chartor.swift
+//  Chartor
+//
+//  Created by Tong Tian on 1/23/18.
+//  Copyright © 2018 Bizersoft. All rights reserved.
+//
+
+import Foundation
+
+class Chartor {
+    
+}
