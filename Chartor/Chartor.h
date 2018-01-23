8e5dfb0f3f0d467a31e80d6393538871b191995a
ttionn <matt.tian@icloud.com>
2018-01-23 22:12:58 +0800

    Initial Commit

---
+++ Chartor/Chartor.h
@@ -0,0 +1,19 @@
+//
+//  Chartor.h
+//  Chartor
+//
+//  Created by Tong Tian on 1/23/18.
+//  Copyright © 2018 Bizersoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Chartor.
+FOUNDATION_EXPORT double ChartorVersionNumber;
+
+//! Project version string for Chartor.
+FOUNDATION_EXPORT const unsigned char ChartorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chartor/PublicHeader.h>
+
+
